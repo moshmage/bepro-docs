812e2370f966254c160dd7bf8646e5d7983b2367
Tiago Moreira <36547913+D45putspin@users.noreply.github.com>
2021-11-18 19:30:52 +0000

    web/mobile design (#1)
    merge design by @D45putspin

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
     <nav-bar />
-    <div class="d-flex">
-      <side-bar class="w-15" :items="navList"></side-bar>
-      <div class="w-85">
+    <div v-if="!isMob" class="d-flex wrapper">
+      <side-bar
+        class="w-25 sticky-top backgroundSideBar"
+        aria-orientation="vertical"
+        style="overflow: scroll; height: 100vh"
+        :items="navList"
+      ></side-bar>
+
+      <div class="backgroundBepro" id="backgroundBepro">
+        <router-view />
+      </div>
+    </div>
+    <!--  mobile v  -->
+    <div class="mobile" v-if="isMob">
+      <side-bar
+        id="backgroundSideBar"
+        v-if="isMob"
+        class="w-100 pt-0 backgroundSideBar"
+        :items="navList"
+      ></side-bar>
+<button class="mr-4 sticky-top btn-menu " id="menu-btn"><img class="img" src="./assets/icn.png" width="20" height="20" ></button>
+      <div class="backgroundBepro" id="backgroundBepro">
         <router-view />
       </div>
     </div>
@@ -11,16 +30,21 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import SideBar from '@components/side-bar.vue';
-import {DocumentationService} from '@services/documentation';
-import NavBar from '@components/nav-bar.vue';
+import { Component, Vue } from "vue-property-decorator";
+import SideBar from "@components/side-bar.vue";
+import { DocumentationService } from "@services/documentation";
+import NavBar from "@components/nav-bar.vue";
 
 @Component({
-             components: {NavBar, SideBar}
-           }) export default class extends Vue {
-  loading = false;
+  components: { NavBar, SideBar },
+})
+export default class extends Vue {
+  get isMob(): boolean {
+    return (window as any).isMobile.any;
+  }
+  loading = true;
   navList: string[] = [];
+  hljs = require("highlight.js");
 
   async mounted(): Promise<void> {
     this.loading = true;
@@ -28,11 +52,18 @@ import NavBar from '@components/nav-bar.vue';
     if (!DocumentationService.scopedMembers$.value)
       await DocumentationService.loadDocumentation();
 
-    this.navList
-        .splice(0, this.navList.length,
-                ...Array.from(new Set(Object.entries(DocumentationService.scopedMembers$.value!)
-                                            .map(([name,]) => name))));
-
+    this.navList.splice(
+      0,
+      this.navList.length,
+      ...Array.from(
+        new Set(
+          Object.entries(DocumentationService.scopedMembers$.value!).map(
+            ([name]) => name
+          )
+        )
+      )
+    );
+    this.hljs.highlightAll;
     this.loading = false;
     return;
   }
--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="d-flex navbar p-3 align-items-center">
-    <router-link class="mr-auto d-flex" :to="{name: `home`}">
-      <img src="@/assets/logo.png" class="logo"/>
+  <div class="navbar p-3 w-100 align-items-center">
+    <router-link
+      class="mr-auto d-flex d-none d-lg-block"
+      :to="{ name: `home` }"
+    >
+      <img src="@/assets/logo.png" class="logo" />
       <span class="ml-2 align-self-start subtitle-small">//docs</span>
     </router-link>
+    <div>
+      <button class="btn-warning mr-4" id="theme-switcher">Switch Mode</button>
+      
+    </div>
     <div>
       <a href="https://github.com/bepronetwork/bepro-js">github/bepro-js</a>
     </div>
@@ -11,17 +18,39 @@
 </template>
 
 <script lang="ts">
-import {Component, Emit, Prop, Vue, Watch} from 'vue-property-decorator';
+import { Component, Emit, Prop, Vue, Watch } from "vue-property-decorator";
 
-@Component export default class NavBar extends Vue {
+@Component
+export default class NavBar extends Vue {}
+// Wait for document to load
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.documentElement.setAttribute("data-theme", "light");
 
-}
+  // Get our button switcher
+  var themeSwitcher = document.getElementById("theme-switcher");
+
+  // When our button gets clicked
+  if (themeSwitcher) {
+    themeSwitcher.onclick = function () {
+      // Get the current selected theme, on the first run
+      // it should be `light`
+      var currentTheme = document.documentElement.getAttribute("data-theme");
+
+      // Switch between `dark` and `light`
+      var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+
+      // Set our currenet theme to the new one
+      document.documentElement.setAttribute("data-theme", switchToTheme);
+    };
+  }
+});
 </script>
 
 <style scoped lang="scss">
 .navbar {
   background-color: #0d0f19;
   color: white;
+  height: 4rem;
 }
 
 .logo {
